f65bfae380f0e6d778a45685bf917eff568ade67
Ray Thurne Void <ray.thurne.void@gmail.com>
2021-09-14 12:34:05 +0200

    0.0.6: fix styleList

--- src/lib/styleList.ts
+++ src/lib/styleList.ts
@@ -2,7 +2,7 @@ import { customList } from "./customList.js";
 import type { StringList } from "./types.js";
 
 export function styleList(toParse: StringList): string | undefined {
-	const parsed = customList(toParse, { join: ": ", end: ";" });
+	const parsed = customList(toParse, { join: "; ", end: ";" });
 
 	return parsed;
 }
